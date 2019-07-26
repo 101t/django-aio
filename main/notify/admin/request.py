4e21a28724e45e22f344b5adf05db28231da54c1
Tarek Kalaji <tarek.it.eng@gmail.com>
2019-07-26 16:45:27 +0300

    remove user role

--- main/notify/admin/request.py
+++ main/notify/admin/request.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals, absolute_import
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
+from django.db.models import Q
 
 from main.users.models import User
 
@@ -14,6 +15,6 @@ class NotificationRequestAdmin(admin.ModelAdmin):
 	list_display = ("title", "user", "created",)
 	search_fields = ("title", "user__username", "user__first_name", "user__last_name", "user__email",)
 	def render_change_form(self, request, context, *args, **kwargs):
-		context['adminform'].form.fields['user'].queryset = User.objects.exclude(role=User.STUDENT)
-		context['adminform'].form.fields['users'].queryset = User.objects.filter(role=User.STUDENT)
+		context['adminform'].form.fields['user'].queryset = User.objects.exclude(is_staff=False, is_superuser=False)
+		context['adminform'].form.fields['users'].queryset = User.objects.filter(is_staff=True, is_superuser=True)
 		return super(NotificationRequestAdmin, self).render_change_form(request, context, *args, **kwargs)
